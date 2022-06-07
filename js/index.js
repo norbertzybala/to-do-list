b083651e5b208ee0cbfc7d858fbea2c907e7690a
Norbert Zybała <zybi1515@gmail.com>
2022-06-07 17:35:25 +0200

    change alert language

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const checkbox = document.querySelectorAll('.todo__checkbox');
 
 checkbox.forEach(item => {
     item.addEventListener('click', () => {
-        item.parentElement.lastElementChild.value ? item.classList.toggle('complete') : alert('Uzupełnij to pole!');
+        item.parentElement.lastElementChild.value ? item.classList.toggle('complete') : alert('This field is empty');
     })
 })
